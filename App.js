11433b7182b2fdf83ff0463060b5b24aeca602da
Kwadwo-900 <63855186+Kwadwo-900@users.noreply.github.com>
2021-10-26 02:07:42 +0000

    update

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from "./screens/login.js";
+
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavigationContainer screenOptions={{ headerShown: true ,  }}>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login}/>
+        </Stack.Navigator>
       <StatusBar style="auto" />
+      </NavigationContainer>
     </View>
   );
 }
@@ -14,8 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#dfd',
     justifyContent: 'center',
   },
 });
---
+++ screens/login.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { StyleSheet, Text, View ,Image,TextInput,TouchableOpacity} from 'react-native';
+import { AntDesign } from "@expo/vector-icons";
+
+export default function Login() {
+    return (
+      <View style={{
+        backgroundColor: "#f57009",
+        flex: 1,
+        paddingHorizontal: 20,
+        justifyContent: "center",
+        alignItems: "center"}}>
+          <Text style={{paddingBottom:"50px", fontWeight:"bold", fontSize:"30"}}>Welcome To Vintage Bikes</Text>
+          <Image
+        style={{
+          transform: [{ rotate: "5deg" }],
+          borderRadius: 20,
+          marginBottom: 60,
+          width: 200,
+          height: 200,
+        }}
+        source={{
+          uri: "https://image.shutterstock.com/image-photo/vintage-bicycle-on-roadside-black-600w-308760134.jpg",
+        }}
+      />
+
+<TextInput 
+ style={{ paddingHorizontal: 60, borderWidth : 0.5, padding:10,borderRadius:10, paddingTop:10}} placeholder="Enter username or email"/>
+ <Text>     </Text>
+
+ <TextInput 
+ style={{ paddingHorizontal: 60, borderWidth : 0.5, padding:10,borderRadius:10, }} placeholder="password"/>
+
+<TouchableOpacity
+       /* onPress={() => {
+          navigation.navigate("Home");
+        }}
+        */
+        style={{
+          padding: 15,
+          paddingHorizontal: 80,
+          marginTop: 10,
+          alignItems: "center",
+          borderRadius: 10,
+          flexDirection: "row",
+          backgroundColor: "black",
+        }}
+      >
+        <AntDesign name="apple1" size={24} color="white" />
+        <Text style={{ paddingLeft: 10, color: "white" }}>Login with AppleIDs</Text>
+      </TouchableOpacity>
+
+
+      </View>
+    );
+  }
+
+  
