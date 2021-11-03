7b1367876aa034f16c315202772d4786f8e2f732
Kwadwo-900 <63855186+Kwadwo-900@users.noreply.github.com>
2021-11-03 04:08:29 +0000

    update

---
+++ screens/sign_up_page.js
@@ -0,0 +1,15 @@
+import react from "react";
+import { StyleSheet, Text, View ,Image,TextInput,TouchableOpacity,ImageBackground} from 'react-native';
+
+export default function sign_up(){
+    return(
+
+    )
+}
+
+const styles = StyleSheet.create({
+    main:{
+        flex:1,
+
+    }
+})
