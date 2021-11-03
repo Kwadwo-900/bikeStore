342c04fa194cce06a7c96b0f59ca6c06c8db070f
Kwadwo-900 <63855186+Kwadwo-900@users.noreply.github.com>
2021-11-03 04:25:48 +0000

    update

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from "./screens/login.js";
 import Home from "./screens/home";
 import landing_page from "./screens/landing_page";
+import sign_up from "./screens/sign_up_page";
+import { Ionicons } from '@expo/vector-icons';
 
 
 
@@ -15,11 +17,22 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <NavigationContainer screenOptions={{ headerShown: true ,  }}>
+      <NavigationContainer screenOptions={{ headerShown:true }}>
         <Stack.Navigator>
-          <Stack.Screen name = "landing_page" component={landing_page}/>
-          <Stack.Screen name="Login" component={Login}/>
-          <Stack.Screen name="Home" component={Home}/>
+          <Stack.Screen name = "landing_page" component={landing_page} options={{title:"WELCOME"}}/>
+          <Stack.Screen name="Login" component={Login} options={{title:"LOGIN"}}/>
+          <Stack.Screen name="Sign_up" component={sign_up} options={{title:"SIGN UP"}}/>
+          <Stack.Screen name="Home" component={Home} 
+          options={{title:"SELECT BIKE",
+        headerLeft:{{onPress}} => {
+          <TouchableOpacity 
+          style={{marginLeft: SIZE.padding}} onPress={onPress}>
+            <Ionicons name="md-menu-sharp" size={24} color="black" />
+            //<Image source={{uri:""}}/>
+
+          </TouchableOpacity>
+        }
+        }} />
         </Stack.Navigator>
       <StatusBar style="auto" />
       </NavigationContainer>
