b054424c9ef760eb949f481ccf9f72e78ca099a4
Kwadwo-900 <63855186+Kwadwo-900@users.noreply.github.com>
2021-11-16 16:30:06 +0000

    Update home.js

--- screens/home.js
+++ screens/home.js
@@ -1,45 +1,53 @@
 import  React,{useState} from'react';
 import { StyleSheet, Text, View ,Image,TextInput,TouchableOpacity} from 'react-native';
-import {images} from constants;
+import {images} from 'constants';
 
-export default function Home({navigation}){
+const Home = ({navigation}) => {
     const [showAddToCart, setAddToCart] = useState(false)
-    const [selectedItem, setSelectedItem] = u
-    seState(null)
+    const [selectedItem, setSelectedItem] = useState(null)
 
     //dummy data to store items 
     const [trending, setTrending] = React.useState([
         {
             id: 0,
             name: "Bike 1",
-            img: images.nikePegasus36,
-            bgColor: "#BF012C",
-            type: "RUNNING",
+            img: images.blackBike,
+            type: "Old",
             price: "$186",
         },
         {
             id: 1,
             name: "Bike 2",
-            img: images.nikeMetcon5Black,
-            bgColor: "#D39C67",
-            type: "TRAINING",
+            img: images.Mbike,
+            type: "Old"
             price: "$135",
            
         },
         {
             id: 2,
             name: "Bike 3",
-            img: images.nikeZoomKobe1Proto,
-            bgColor: "#7052A0",
-            type: "BASKETBALL",
+            img: images.Wbike,
+            type: "Old",
             price: "$199",
         },
     ]);
-    return(
-        <View>
-            
-            
-        </View>
+   function renderItems(item,index){
+       var bikeView = {}
+       if(index == 0){
+           bikeView = {marginLeft:24}
+       } else {bikeView = {} }
 
-    );
+       return (
+        <TouchableOpacity
+        style={{ height: 240, width: 180, justifyContent: 'center', marginHorizontal: 8, ...bikeVieW }}
+        onPress={() => {
+            setSelectedItem(item)
+            setAddToCart(true)
+        }}
+    >
+        <Text style={{ color: COLORS.gray, ...FONTS.h5 }}>{item.type}</Text>
+       )
+   }
 }
+
+export default Home;
