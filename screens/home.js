106e2d65dfd1a56da4cfeb10655f7868367a9506
Kwadwo-900 <63855186+Kwadwo-900@users.noreply.github.com>
2021-11-17 11:20:42 +0000

    removal of home screen

--- screens/home.js
+++ screens/home.js
@@ -1,6 +1,6 @@
 import  React,{useState} from'react';
 import { StyleSheet, Text, View ,Image,TextInput,TouchableOpacity} from 'react-native';
-import {images} from 'constants';
+import {images,COLORS, FONTS, SIZES} from "../constants";
 
 const Home = ({navigation}) => {
     const [showAddToCart, setAddToCart] = useState(false)
@@ -19,7 +19,7 @@ const Home = ({navigation}) => {
             id: 1,
             name: "Bike 2",
             img: images.Mbike,
-            type: "Old"
+            type: "Old",
             price: "$135",
            
         },
@@ -31,23 +31,97 @@ const Home = ({navigation}) => {
             price: "$199",
         },
     ]);
-   function renderItems(item,index){
-       var bikeView = {}
-       if(index == 0){
-           bikeView = {marginLeft:24}
-       } else {bikeView = {} }
+    function renderTrendingShoes(item, index) {
+        var trendingStyle = {};
 
-       return (
-        <TouchableOpacity
-        style={{ height: 240, width: 180, justifyContent: 'center', marginHorizontal: 8, ...bikeVieW }}
-        onPress={() => {
-            setSelectedItem(item)
-            setAddToCart(true)
-        }}
-    >
-        <Text style={{ color: COLORS.gray, ...FONTS.h5 }}>{item.type}</Text>
-       )
-   }
+        if (index == 0) {
+            trendingStyle = { marginLeft: SIZES.padding, }
+        } else {
+            trendingStyle = {}
+        }
+
+        return (
+            <TouchableOpacity
+                style={{ height: 240, width: 180, justifyContent: 'center', marginHorizontal: SIZES.base, ...trendingStyle }}
+                onPress={() => {
+                    setSelectedItem(item)
+                    setShowAddToBagModal(true)
+                }}
+            >
+                <Text style={{ color: COLORS.gray, ...FONTS.h5 }}>{item.type}</Text>
+
+                <View style={[{
+                    flex: 1,
+                    justifyContent: 'flex-end',
+                    marginTop: SIZES.base,
+                    borderTopLeftRadius: 10,
+                    borderBottomLeftRadius: 10,
+                    borderBottomRightRadius: 10,
+                    marginRight: SIZES.padding,
+                    paddingLeft: SIZES.radius,
+                    paddingRight: SIZES.padding,
+                    paddingBottom: SIZES.radius,
+                    backgroundColor: item.bgColor
+                }, styles.trendingShadow]}>
+                    <View style={{ height: '35%', justifyContent: 'space-between' }}>
+                        <Text style={{ color: COLORS.white, ...FONTS.body4 }}>{item.name}</Text>
+                        <Text style={{ color: COLORS.white, ...FONTS.h3 }}>{item.price}</Text>
+                    </View>
+                </View>
+
+                <View style={{ position: 'absolute', top: 27, right: 0, width: "95%", height: "100%" }}>
+                    <Svg height="100%" width="100%">
+                        <Polygon
+                            points="0,0 160,0 160,80"
+                            fill="white"
+                        />
+                    </Svg>
+                </View>
+
+                <Image
+                    source={item.img}
+                    resizeMode="cover"
+                    style={{
+                        position: 'absolute',
+                        top: 50,
+                        right: 0,
+                        width: "98%",
+                        height: 80,
+                        transform: [
+                            { rotate: '-15deg' }
+                        ]
+                    }}
+                />
+            </TouchableOpacity>
+        )
+    }
+
+    function renderRecentlyViewed(item, index) {
+        return (
+            <TouchableOpacity
+                style={{ flex: 1, flexDirection: 'row' }}
+                onPress={() => {
+                    setSelectedItem(item)
+                    setShowAddToBagModal(true)
+                }}
+            >
+                <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                    <Image
+                        source={item.img}
+                        resizeMode="contain"
+                        style={{
+                            width: 130,
+                            height: 100,
+                        }}
+                    />
+                </View>
+                <View style={{ flex: 1.5, marginLeft: SIZES.radius, justifyContent: "center" }}>
+                    <Text style={{ color: COLORS.gray, ...FONTS.body3 }}>{item.name}</Text>
+                    <Text style={{ ...FONTS.h3 }}>{item.price}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
 }
 
 export default Home;
